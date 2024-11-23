ed3c634ea0551ac2f3bf76b874efc30ee427eef5
Ajeet <choubeyajeet0@gmail.com>
2024-11-23 14:15:12 +0530

    Created the Attributes and constructor of Arena Class. Created an object of Arena class in main method.

--- src/main/java/org/example/Arena.java
+++ src/main/java/org/example/Arena.java
@@ -1,5 +1,15 @@
 package org.example;
 
-public class Arena {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Arena {
+    private int totalPlayers;
+    private Map<Integer, Player> players;
+
+    public Arena() {
+        this.totalPlayers = 0;
+        this.players = new HashMap<>();
+        System.out.println("--------Welcome to the Arena--------");
+    }
 }
